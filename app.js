6164a35434cf5e6959cdebb413e1684b3326a0aa
dEV <pydev21@gmail.com>
2019-04-03 12:17:35 +0530

    add env

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //jshint esversion:6
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -19,8 +21,8 @@ const userSchema = new mongoose.Schema({
     password:String, 
 });
 
-const secret = "Devisawesomebutheissupercool!"
-userSchema.plugin(encrypt, { secret: secret, encryptedFields:["password"] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"] });
 
 
 
